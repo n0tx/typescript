c17c26b4f48522abc004acfcf1586ba07cbb502f
n0tx <rcandra91@msn.com>
2025-05-30 20:41:50 +0700

    tipe data array

---
+++ array.js
@@ -0,0 +1,7 @@
+// array menggunakan type annotation
+var daftarNilai = [80, 75, 90, 65];
+console.log({ daftarNilai: daftarNilai });
+// array menggunakan otomatis type annotation dengan
+// melalui dari isi data variable
+var daftarNama = ["Riki", "Andi", "Budi", "Dery"];
+console.log({ daftarNama: daftarNama });
---
+++ array.ts
@@ -0,0 +1,8 @@
+// array menggunakan type annotation
+let daftarNilai: number[] = [80, 75, 90, 65];
+console.log({ daftarNilai });
+
+// array menggunakan otomatis type annotation dengan
+// melalui dari isi data variable
+let daftarNama = ["Riki", "Andi", "Budi", "Dery"];
+console.log({ daftarNama });
