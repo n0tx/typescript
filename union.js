90a0b070240aaecfa9c329203d716c9bc1e8abc8
n0tx <rcandra91@msn.com>
2025-05-30 19:15:47 +0700

    union

---
+++ union.js
@@ -0,0 +1,8 @@
+// union satu variabel dapat mempunyai
+// 2 tipe data
+var image = "image.png";
+// null itu bertipe data any (belum mempunyai tipe data)
+// dan bisa di masukan nilai dengan tipe data apa saja
+console.log(image);
+image = null;
+console.log(image);
