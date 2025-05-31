7d4fe1f8c81b567c528a7e323d691f3d5735a750
n0tx <rcandra91@msn.com>
2025-05-31 19:31:15 +0700

    fungsi void

---
+++ fungsi_void.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function beriSalam() {
+    var salam = "Halo, selamat datang!";
+    console.log(salam);
+}
+console.log(beriSalam()); // Halo, selamat datang! dan undefined
+beriSalam(); // Halo, selamat datang!
---
+++ fungsi_void.ts
@@ -0,0 +1,9 @@
+export {};
+function beriSalam(): void {
+    let salam: string = "Halo, selamat datang!";
+    console.log(salam);
+}
+
+console.log(beriSalam()); // Halo, selamat datang! dan undefined
+
+beriSalam(); // Halo, selamat datang!
