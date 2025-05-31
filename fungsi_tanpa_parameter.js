d8e787c9ed05299585cec94195b475cca73c27f3
n0tx <rcandra91@msn.com>
2025-05-31 17:03:25 +0700

    fungsi tanpa parameter

---
+++ fungsi_tanpa_parameter.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello() {
+    return "Hello World!";
+}
+// print [Function: hello]
+console.log(hello); // [Function: hello]
+// eksekusi fungsi hello
+console.log(hello()); // Hello World!
---
+++ fungsi_tanpa_parameter.ts
@@ -0,0 +1,9 @@
+export {};
+function hello(): string {
+    return "Hello World!";
+}
+
+// print [Function: hello]
+console.log(hello); // [Function: hello]
+// eksekusi fungsi hello
+console.log(hello()); // Hello World!
