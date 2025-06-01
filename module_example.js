6ac8350355fc17ee01ddb66fc44700dd2a8320b1
n0tx <rcandra91@msn.com>
2025-06-02 01:47:08 +0700

    module export import

---
+++ module_example.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MathUtils_1 = require("./utils/MathUtils");
+console.log((0, MathUtils_1.add)(2, 5));
+console.log((0, MathUtils_1.multiple)(5, 5));
---
+++ module_example.ts
@@ -0,0 +1,4 @@
+import { add, multiple } from "./utils/MathUtils";
+
+console.log(add(2, 5));
+console.log(multiple(5, 5));
